6bd5fb3d933c7d93206bb2864dbc2536fdf22f5e
Maykon R. Meier <maykon.meier@gmail.com>
2018-03-05 14:36:02 +0000

    Adicionado contador de contrutor no retorno da view

--- UsuarioController.cs
+++ UsuarioController.cs
@@ -1,12 +1,14 @@
 namespace Helios.Web.Controller {
 	public class UsuarioController : BaseController {
-		private int _count = 0;
+		private static int _count = 0;
+		private static string _constructorCount = 0;
 
 		public UsuarioController() {
+			this._constructorCount++;
 		}
 
 		public JsonResult Index() {
-			return View("Inicio");
+			return View("Inicio", new { ConstructorCount = this._constructorCount });
 		}
 	}
 }
